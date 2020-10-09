26b948a52b3bdca3c98d84d92b91cea6f63e7a1f
ranon rat <66473662+ranon-rat@users.noreply.github.com>
2020-10-08 21:36:50 -0500

    Add files via upload

--- encription.go
+++ encription.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"fmt"
-	"strconv"
 	"strings"
 )
 
@@ -18,7 +17,7 @@ var (
 	c       []string
 	en      []string
 	des     []string
-	qwerty  []string = strings.Split("qwertyuiopasdfghjklñzxcvbnm @!#$%&/()=|°", "")
+	qwerty  []string = strings.Split("qwertyuiopasdfghjklñzxcvbnm 1234567890@!#$%&/()=|°", "")
 )
 
 func cesar(character string, seed int) string {
@@ -38,44 +37,40 @@ func resolveCesar(character string, seed int) string { //cifrade ceaser
 		if qwerty[q] == character {
 			for q < s {
 				if q == 0 {
-					s = s - 1
+					s--
 				} else {
 					s -= q
 				}
 			}
+			s %= len(qwerty)
 			characterDes = qwerty[q-s]
 		}
 	}
 	return characterDes
 }
 func (e *encriptado) enAndDes(character string, seed int) {
-
 	encript += cesar(character, seed)
 	m := strings.Split(encript, "")
 	resolve += resolveCesar(m[len(m)-1], seed)
 }
-func (e *encriptado) codeCon(text string, contraseña string) {
+func (e *encriptado) codeCon(text string, patron string) {
 	t = strings.Split(text, "")
-	c = strings.Split(contraseña, "")
+	c = strings.Split(patron, "")
 	x := 0
-	for x < len(text)%len(c) { //password
-		for _, i := range t { //text
+	for x < len(c) { //password
+		for i := 0; i < len(t); i++ { //text
 			for y := 0; y < len(qwerty); y++ { // qwerty for return
-				if c[x%len(c)] == qwerty[y] {
-					e.enAndDes(i, y)
-				}
-				valor, err := strconv.Atoi(c[x%len(c)])
-				if err == nil {
-					e.enAndDes(i, valor)
+				if c[x] == qwerty[y] {
+					e.enAndDes(t[i], y)
 				}
 			}
 			e.encriptList = append(e.encriptList, encript)
 			e.resolveList = append(e.resolveList, resolve)
 			resolve = ""
 			encript = ""
-			x++
-		}
 
+		}
+		x++
 	}
 }
 
@@ -86,20 +81,14 @@ func unique(text []string) []string {
 		if occured[eS] != true {
 			occured[eS] = true
 			t = append(t, eS)
-
 		}
 	}
 	return t
-
 }
-
 func main() {
-
 	e := encriptado{}
-	m := strings.Join(qwerty, " ")
-	fmt.Println(m)
-	e.codeCon(m, "pato asado ")
-	fmt.Println(unique(e.encriptList))
-	fmt.Println(unique(e.resolveList))
-
+	m := " puta puta ptua puta puta puta"
+	e.codeCon(m, "scidroid es homosexual")
+	fmt.Println(e.encriptList)
+	fmt.Println(e.resolveList)
 }
